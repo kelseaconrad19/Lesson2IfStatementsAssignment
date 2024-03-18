18edb4e1604b80b6960b1f647c4cdb31fe18a51a
kelseaconrad19 <163567548+kelseaconrad19@users.noreply.github.com>
2024-03-18 12:27:39 -0400

    Update TheGreatestShowdown.py
    Changed based on feedback.

--- TheGreatestShowdown.py
+++ TheGreatestShowdown.py
@@ -10,7 +10,7 @@ largest = num_1
 # Find the largest number
 if num_2 > largest:
     largest = num_2
-elif num_3 > largest:
+if num_3 > largest:
     largest = num_3
 
 # Task 2: Extend your program from Task 1 to also determine the smallest number among the three and print it out.
@@ -18,11 +18,11 @@ smallest = num_1
 # Find the smallest number
 if num_2 < smallest:
     smallest = num_2
-elif num_3 < smallest:
+if num_3 < smallest:
     smallest = num_3
 
 # Task 3: Enhance your program to handle cases where two or all of the numbers are equal. The program should display appropriate messages like "Two numbers are equal and the largest" or "All numbers are equal".
 if num_1 == num_2 and num_1 == num_3:
     print("All numbers are equal.")
-elif num_1 == num_2 or num_1 == num_3:
+if num_1 == num_2 or num_1 == num_3:
     print(f"The smallest number is {smallest}. The Largest number is {largest}. There are two numbers equal to each other.")
